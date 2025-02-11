51e337f54d7a7bbe137c136f763921059fad735e
molka kammoun <molkakammoun@gmail.com>
2025-02-11 15:19:57 +0100

    modified

--- src/components/listgroup.tsx
+++ src/components/listgroup.tsx
@@ -1,16 +1,19 @@
+import { MouseEvent } from "react";
 function listgroup() {
   let items = ["london", "paris", "tunis", "newyork", "tokyo"];
+
+  const handleClick=(event: MouseEvent)=>console.log(Event) ;
 
   return (
     <>
       <h1>liste</h1>
       {items.length == 0 ? <p>no item found</p> : null}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item,index) => (
           <li
-            className="list-group-item"
+            className="list-group-item active"
             key={item}
-            onClick={() => console.log(item)}
+            onClick={handleClick}
           >
             {item}
           </li>
