d5e9f202723aab4f891e8801f95623d4509615b2
molka kammoun <molkakammoun@gmail.com>
2025-02-11 14:32:39 +0100

    commit4

--- src/components/listgroup.tsx
+++ src/components/listgroup.tsx
@@ -1,10 +1,11 @@
 function listgroup() {
-  const items = ["london", "paris", "tunis", "newyork", "tokyo"];
-  items.map((item) => <li>{item}</li>);
-
+  let items = ["london", "paris", "tunis", "newyork", "tokyo"];
+  items=[];
+  
   return (
     <>
       <h1>liste</h1>
+      {items.length==0?<p>no item found</p>:null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
